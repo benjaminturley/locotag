664dead65388bee3a160bc373a69e84adadd3b0a
Benjamin Turley <bjam.turley@gmail.com>
2016-07-15 02:08:11 -0400

    Added physical tag object and some art components
    -Added tag, help, and donate images
    -Created springy string for tag
    -Added DragRigidbody2D and FreezeRotation scripts
    -Added NotoSans font

---
+++ Assets/Scripts/Rigidbody2DFreezeRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Rigidbody2DFreezeRotation : MonoBehaviour 
+{
+	void Start () 
+	{
+		GetComponent<Rigidbody2D> ().freezeRotation = true;
+	}
+}
